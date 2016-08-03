d697adf6f7f8df2a174a7606a1484f54ad9f5957
MishaHerscu <mishaherscu@gmail.com>
2016-08-03 11:34:44 -0400

    edited trip serializer

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,6 +1,6 @@
 #
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :start_date, :end_date, :attendances
+  attributes :id, :city_id, :user_id, :start_date, :end_date, :attendances
   has_one :user
   has_one :city
 
