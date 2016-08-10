abb180718b4e4c5e70a01097d4738b2b5e66f324
MishaHerscu <mishaherscu@gmail.com>
2016-08-10 16:47:34 -0500

    comment out making an attendance when making a trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -24,7 +24,7 @@ class TripsController < ProtectedController
     @trip = Trip.new(trip_params)
     @trip.user_id = @current_user.id
     if @trip.save
-      Attendance.create(user_id: @current_user.id, trip_id: @trip.id)
+      # Attendance.create(user_id: @current_user.id, trip_id: @trip.id)
       render json: @trip, status: :created, location: @trip
     else
       render json: @trip.errors, status: :unprocessable_entity
