24bbe39de16d0490437436d32d9fce56e133b4f2
MishaHerscu <mishaherscu@gmail.com>
2017-03-12 12:47:39 -0500

    attraction serializer update

--- app/serializers/attraction_serializer.rb
+++ app/serializers/attraction_serializer.rb
@@ -1,10 +1,10 @@
 #
 class AttractionSerializer < ActiveModel::Serializer
-  attributes :id, :eventful_id, :city, :city_name, :country_name, :title,
-             :description, :keywords_string, :owner, :db_start_time,
-             :db_stop_time, :event_date, :event_time, :event_time_zone,
-             :all_day, :venue_id, :venue_name, :venue_address, :postal_code,
-             :venue_url, :geocode_type, :latitude, :longitude,
+  attributes :id, :categories_string, :eventful_id, :city, :city_id, :city_name,
+             :country_name, :title, :description, :keywords_string, :owner,
+             :db_start_time, :db_stop_time, :event_date, :event_time,
+             :event_time_zone, :all_day, :venue_id, :venue_name, :venue_address,
+             :postal_code, :venue_url, :geocode_type, :latitude, :longitude,
              :image_information, :medium_image_url, :attraction_suggestions,
              :attraction_categories
 
